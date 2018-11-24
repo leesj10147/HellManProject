93aba9757530cc87f29da17eb3e8568321dc0f4c
이 승재 <fkddl1436@naver.com>
2018-11-24 21:05:29 +0900

    log프린트 지움

--- src/core/Game.java
+++ src/core/Game.java
@@ -56,7 +56,6 @@ public final class Game extends Canvas implements Runnable
                 tick();
                 render();
                 --delta;
-                System.out.println(delta);
             }
         }
     }
