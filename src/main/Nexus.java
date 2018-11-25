c41f50594063d8c9930d482429cf9ac8f19ffcbe
이 승재 <fkddl1436@naver.com>
2018-11-25 19:27:23 +0900

    유닛의 가격 결정 readme업데이트

--- src/main/Nexus.java
+++ src/main/Nexus.java
@@ -12,8 +12,8 @@ public class Nexus extends BasicTower implements Battleable
     private transient String[] itemList = new String[10];
     private transient String[] itemDescription = new String[10];
     public transient int gold = 50;
-    public transient int crop = 50;
-    public transient int odamant = 50;
+    public transient int crop = 20;
+    public transient int odamant = 10;
 
     public Nexus(Team team, double x, double y, BufferedImage image, ID id, Handler handler, int renderOrder)
     {
@@ -33,16 +33,16 @@ public class Nexus extends BasicTower implements Battleable
         itemList[8] = "BasicInfantry.png";
         itemList[9] = "BasicInfantry.png";
 
-        itemDescription[0] = "이 친구는 주변의 광산, 농장, 제련소에서 노동을 하며 돈을 벌 수 있습니다.\n 기본적인 공격력과 방어력도 가지고 있지요.";
-        itemDescription[1] = "적절한 사거리와 적절한 데미지를 가지고, 적절한 크기와 함께 적절한 아름다움을 가진 적절한 대포입니다.\n 적절한 가격으로 적절하게 지을 수 있습니다.";
-        itemDescription[2] = "한 대만 맞으면 부셔져 버릴 것 같은 모습과는 다르게, 끝에 도달하기만 한다면... 콰광!";
-        itemDescription[3] = "하나는 약하지만, 모이면 강력합니다!\n3인조로 뭉쳐 다니는 이 골칫거리들은 하는 짓마다 얄밉기짝이 없다고 하더군요.";
-        itemDescription[4] = "그림자같이 은밀하고 빠르게 침투한다고 하여 그림자 침투병이 되었습니다.\n하지만, 걸친 게 천쪼가리뿐이라 너무나도 연약한 존재이기도 합니다.";
-        itemDescription[5] = "사막잡신 \'느킨\'을 믿는 부족의 주술사입니다.\n이교도라고는 하지만, 아군들의 체력을 채워주는 실력은 그 누구도 부정할 수 없을 만큼 확실합니다.";
-        itemDescription[6] = "가로 방어막 입니다.\n아무 공격도 하지 않으며, 길을 막아주는 역할만 합니다.";
-        itemDescription[7] = "세로 방어막 입니다.\n아무 공격도 하지 않으며, 길을 막아주는 역할만 합니다.";
-        itemDescription[8] = "원거리 공격이 가능한 마법사 입니다.\n 그러나 집에 박혀 연구만 한 나머지 느린 이동속도와 약한 채력을 가집니다.";
-        itemDescription[9] = "델막샤의 친위대 시절 혹독한 훈련을 받은 불굴의 기사입니다.\n강한 정신력과 높은 채력을 소지하고 있습니다.";
+        itemDescription[0] = "이 친구는 주변의 광산, 농장, 제련소에서 노동을 하며 돈을 벌 수 있습니다.\n 기본적인 공격력과 방어력도 가지고 있지요.\n 가격 : 10gold";
+        itemDescription[1] = "적절한 사거리와 적절한 데미지를 가지고, 적절한 크기와 함께 적절한 아름다움을 가진 적절한 대포입니다.\n 적절한 가격으로 적절하게 지을 수 있습니다.\n 가격 : 20gold + 10crop + 5odamant";
+        itemDescription[2] = "한 대만 맞으면 부셔져 버릴 것 같은 모습과는 다르게, 끝에 도달하기만 한다면... 콰광!\n 가격 : 10gold + 5crop";
+        itemDescription[3] = "하나는 약하지만, 모이면 강력합니다!\n3인조로 뭉쳐 다니는 이 골칫거리들은 하는 짓마다 얄밉기짝이 없다고 하더군요.\n 가격 : 7gold + 10crop";
+        itemDescription[4] = "그림자같이 은밀하고 빠르게 침투한다고 하여 그림자 침투병이 되었습니다.\n하지만, 걸친 게 천쪼가리뿐이라 너무나도 연약한 존재이기도 합니다.\n 가격 : 10gold + 10crop + 10odamant";
+        itemDescription[5] = "사막잡신 \'느킨\'을 믿는 부족의 주술사입니다.\n이교도라고는 하지만, 아군들의 체력을 채워주는 실력은 그 누구도 부정할 수 없을 만큼 확실합니다.\n 가격 : 8gold + 1odamant";
+        itemDescription[6] = "가로 방어막 입니다.\n아무 공격도 하지 않으며, 길을 막아주는 역할만 합니다.\n 가격 : 7gold + 5crop";
+        itemDescription[7] = "세로 방어막 입니다.\n아무 공격도 하지 않으며, 길을 막아주는 역할만 합니다.\n 가격 : 7gold + 5crop";
+        itemDescription[8] = "원거리 공격이 가능한 마법사 입니다.\n 그러나 집에 박혀 연구만 한 나머지 느린 이동속도와 약한 채력을 가집니다.\n 가격 : 10gold + 3crop";
+        itemDescription[9] = "델막샤의 친위대 시절 혹독한 훈련을 받은 불굴의 기사입니다.\n강한 정신력과 높은 채력을 소지하고 있습니다.\n 가격 : 20gold";
         this.printBottom = false;
     }
 
@@ -123,43 +123,92 @@ public class Nexus extends BasicTower implements Battleable
         {
             if (rect[0].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new MoneyMakerInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[0]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold >= 10)
+                {
+                    handler.addObject(new MoneyMakerInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[0]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 10;
+                }
             } else if (rect[1].contains(mouseClickedOnMapLocation))
             {
-                imageArrangementUnit = itemList[1];
-                selectedArrangementUnit = true;
-                itemNumberOfSelectedUnit = 1;
-                this.mouseClickedOnMapLocation = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+                if (gold >= 20 && crop >= 10 && odamant >= 5)
+                {
+                    imageArrangementUnit = itemList[1];
+                    selectedArrangementUnit = true;
+                    itemNumberOfSelectedUnit = 1;
+                    this.mouseClickedOnMapLocation = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+                    gold -= 20;
+                    crop -= 10;
+                    odamant -= 5;
+                }
             } else if (rect[2].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new SkeletonBombInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[2]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold >= 10 && crop >= 5)
+                {
+                    handler.addObject(new SkeletonBombInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[2]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 10;
+                    crop -= 5;
+                }
             } else if (rect[3].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new GoblinGangInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[3]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold >= 7 && crop >= 10)
+                {
+                    handler.addObject(new GoblinGangInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[3]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 7; crop -= 10;
+                }
             } else if (rect[4].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new ShadowPenetrationInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[4]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold >= 10 && crop >= 10 && odamant >= 10)
+                {
+                    handler.addObject(new ShadowPenetrationInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[4]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 10;
+                    crop -= 10;
+                    odamant -= 10;
+                }
             } else if (rect[5].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new NeukinShamanInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[5]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold>=8 && odamant>=1)
+                {
+                    handler.addObject(new NeukinShamanInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[5]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 8;
+                    odamant -= 1;
+                }
             } else if (rect[6].contains(mouseClickedOnMapLocation))
             {
-                imageArrangementUnit = itemList[6];
-                selectedArrangementUnit = true;
-                itemNumberOfSelectedUnit = 6;
-                this.mouseClickedOnMapLocation = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+                if (gold >= 7 && crop >= 5)
+                {
+                    imageArrangementUnit = itemList[6];
+                    selectedArrangementUnit = true;
+                    itemNumberOfSelectedUnit = 6;
+                    this.mouseClickedOnMapLocation = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+                    gold -= 7;
+                    crop -= 5;
+                }
             } else if (rect[7].contains(mouseClickedOnMapLocation))
             {
-                imageArrangementUnit = itemList[7];
-                selectedArrangementUnit = true;
-                itemNumberOfSelectedUnit = 7;
-                this.mouseClickedOnMapLocation = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+                if (gold >= 7 && crop >= 5)
+                {
+                    imageArrangementUnit = itemList[7];
+                    selectedArrangementUnit = true;
+                    itemNumberOfSelectedUnit = 7;
+                    this.mouseClickedOnMapLocation = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+                    gold -= 7;
+                    crop -= 5;
+                }
             } else if (rect[8].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new MagicianInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[8]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold >= 10 && crop >= 3)
+                {
+                    handler.addObject(new MagicianInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[8]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 10;
+                    crop -= 3;
+                }
             } else if (rect[9].contains(mouseClickedOnMapLocation))
             {
-                handler.addObject(new HeavyArmorInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[8]), ID.Infantry, handler, RenderOrder.Main.order));
+                if (gold >= 20)
+                {
+                    handler.addObject(new HeavyArmorInfantry(team, this.x + this.getWIDTH(), this.y + this.getHEIGHT(), GameManager.loadImage(itemList[8]), ID.Infantry, handler, RenderOrder.Main.order));
+                    gold -= 20;
+                }
             }
             Point p = BattleScene.getOnMapLocation(MouseInput.getLocation());
             boolean contain = false;
@@ -183,6 +232,7 @@ public class Nexus extends BasicTower implements Battleable
 
     private void drawString(Graphics2D g, String text, int x, int y)
     {
+        y += g.getFontMetrics().getHeight();
         for (String line : text.split("\n"))
             g.drawString(line, x, y += g.getFontMetrics().getHeight());
     }
